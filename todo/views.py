2b8ba899344f66e3d8802739a830ac093402a8d4
GurraT <gustafthomsson@hotmail.com>
2021-04-18 10:48:28 +0000

    Add templates, url, CR-function to db

---
+++ todo/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from .models import Items
+
+# Create your views here.
+
+
+def get_todo_list(request):
+    items = Items.objects.all()
+    context = {
+        "items": items
+    }
+    return render(request, 'todo/todo_list.html', context)
+
+
+def add_item(request):
+    if request.method == "POST":
+        name = request.POST.get('item_name')
+        done = 'done' in request.POST
+        Items.objects.create(name=name, done=done)
+        
+        return redirect('get_todo_list')
+    return render(request, 'todo/add_item.html')
